7acea47aefa73eb8947190fdaf170a912558eda0
Mateusz Olszewski <mateusz.k.olszewski@student.put.poznan.pl>
2022-07-20 17:49:20 +0200

    init

---
+++ src/main/java/com/example/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
