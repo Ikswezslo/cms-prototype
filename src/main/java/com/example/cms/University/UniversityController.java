faf38ec4af6fcb334c890a2b859a0ca10ee63e41
Mielczarek-wiz <mielczarek.damian99@o2.pl>
2022-08-11 21:28:49 +0200

    Refactoring "S"

--- src/main/java/com/example/cms/University/UniversityController.java
+++ src/main/java/com/example/cms/University/UniversityController.java
@@ -1,6 +1,7 @@
 package com.example.cms.University;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "university")
+@RequestMapping(path = "universities")
 public class UniversityController {
     private final UniversityService universityService;
 
