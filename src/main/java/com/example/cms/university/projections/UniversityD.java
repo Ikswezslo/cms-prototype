c1838bd4614a160096c13d107e78a77db63b455d
Mielczarek-wiz <mielczarek.damian99@o2.pl>
2022-08-27 18:05:25 +0200

    Added Set of users to UniversityD

--- src/main/java/com/example/cms/university/projections/UniversityD.java
+++ src/main/java/com/example/cms/university/projections/UniversityD.java
@@ -1,7 +1,10 @@
 package com.example.cms.university.projections;
 
 import com.example.cms.university.University;
+import com.example.cms.user.User;
 import lombok.Value;
+
+import java.util.Set;
 
 @Value
 public class UniversityD {
@@ -10,11 +13,14 @@ public class UniversityD {
     String shortName;
     boolean hidden;
 
+    Set<User> enrolledUsers;
+
     public UniversityD(University university) {
         this.id = university.getId();
         this.name = university.getName();
         this.shortName = university.getShortName();
         this.hidden = university.isHidden();
+        this.enrolledUsers = university.getEnrolledUsers();
     }
 
 }
