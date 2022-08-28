421e5ec44ede1b4bc394ad905f69480a6eb11d5c
Vi-Moczulak <69606317+Vi-Moczulak@users.noreply.github.com>
2022-08-28 20:30:44 +0200

    walidacja pól w dialogu dodania uzytkownika

--- src/main/resources/Angular/src/app/dialog-user-create/dialog-user-create.component.ts
+++ src/main/resources/Angular/src/app/dialog-user-create/dialog-user-create.component.ts
@@ -13,29 +13,40 @@ import { UserService } from 'src/assets/service/user.service';
 export class DialogUserCreateComponent implements OnInit {
 
   readonly user = {} as UserForm;
-  email = new FormControl('', [Validators.required, Validators.email]);
+  usernameValid = new FormControl('', [Validators.required]);
+  passwordValid = new FormControl('', [Validators.required]);
+  accountTypeValid = new FormControl('', [Validators.required]);
+  emailValid = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
 
   constructor(
     public dialog: MatDialog,
     private userService: UserService,
     public dialogRef: MatDialogRef<DialogUserCreateComponent>,
-  ) {
-    
-  }
+  ) {}
 
   ngOnInit(): void {
   }
 
   createUser(): void {
     console.log(this.user);
-    this.userService.createUser(this.user).subscribe({
-      next: user => console.log(user),
-      error: error => RestErrorHandler.handleError(RestErrorHandler.prepareError(error))
-    });
-    this.close();
+    if (this.usernameValid.status == "VALID" && this.passwordValid.status == "VALID" &&
+        this.emailValid.status == "VALID" && this.accountTypeValid.status == "VALID") {
+      this.userService.createUser(this.user).subscribe({
+        next: user => console.log(user),
+        error: error => RestErrorHandler.handleError(RestErrorHandler.prepareError(error))
+      });
+      this.close();
+    } 
   }
+
   close() {
     this.dialog.closeAll()
+  }
+
+  getErrorMessage() {
+    if (this.emailValid.hasError('required')) return 'You must enter a value';
+    else if (this.emailValid.hasError('email')) return 'Not a valid email';
+    return '';
   }
 }
