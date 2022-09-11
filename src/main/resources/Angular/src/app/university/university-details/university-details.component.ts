74dfa19c1c7f872d7966ba448c6b4e2b8fdba179
Mielczarek-wiz <mielczarek.damian99@o2.pl>
2022-09-11 18:36:20 +0200

    Added limit for university (USER cannot hide or unhide university)

--- src/main/resources/Angular/src/app/university/university-details/university-details.component.ts
+++ src/main/resources/Angular/src/app/university/university-details/university-details.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {University} from 'src/assets/models/university';
 import {UniversityService} from 'src/assets/service/university.service';
+import {User} from "../../../assets/models/user";
+import {UserService} from "../../../assets/service/user.service";
 
 @Component({
   selector: 'app-university-details',
@@ -12,17 +14,20 @@ export class UniversityDetailsComponent implements OnInit {
 
   public university!: University;
   public id: Number = 0;
+  public loggedUser!: User;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private universityService: UniversityService) {
+    private universityService: UniversityService,
+    private userService: UserService) {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     this.id = Number(routeParams.get('universityId'));
+    this.getLoggedUser();
     this.loadUniversity();
   }
 
@@ -37,9 +42,17 @@ export class UniversityDetailsComponent implements OnInit {
   hiddenUniversity() {
     this.universityService.hideUniversity(this.university.id, !this.university.hidden).subscribe(() => {
       this.loadUniversity();
-    })
+    });
 
   }
+
+  getLoggedUser() {
+    this.userService.getLoggedUser()
+      .subscribe(res => {
+        this.loggedUser = res;
+      })
+  }
+
 }
 
 
