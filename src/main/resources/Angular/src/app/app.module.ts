6607b1c835dc5375ba6e263c43d42cac64bfa9ba
Mateusz Olszewski <mateusz.k.olszewski@student.put.poznan.pl>
2022-10-06 13:00:12 +0200

    Add success-dialog

--- src/main/resources/Angular/src/app/app.module.ts
+++ src/main/resources/Angular/src/app/app.module.ts
@@ -58,6 +58,7 @@ import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {
   DialogUserChangePasswordComponent
 } from './user/dialogs/dialog-user-change-password/dialog-user-change-password.component';
+import {SuccessDialogComponent} from './dialog/success-dialog/success-dialog.component';
 
 @NgModule({
   declarations: [
@@ -85,6 +86,7 @@ import {
     SpinnerComponent,
     DialogUserAddUniversityComponent,
     DialogUserChangePasswordComponent,
+    SuccessDialogComponent,
   ],
   imports: [
     BrowserModule,
