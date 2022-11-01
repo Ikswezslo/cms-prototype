844cd573aac6134a17d8b9dd339448a40478625e
Mielczarek-wiz <mielczarek.damian99@o2.pl>
2022-11-01 18:30:50 +0100

    Integrated with PostgreSQL

--- src/main/java/com/example/cms/user/UserRepository.java
+++ src/main/java/com/example/cms/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.cms.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
