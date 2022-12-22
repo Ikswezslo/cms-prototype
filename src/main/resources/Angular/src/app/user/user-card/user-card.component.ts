e9c76906788bc8aac985ac0cd405df8fdb8690e6
Mateusz Olszewski <mateusz.k.olszewski@student.put.poznan.pl>
2022-12-22 02:17:51 +0100

    Translate accountType in user-card

--- src/main/resources/Angular/src/app/user/user-card/user-card.component.ts
+++ src/main/resources/Angular/src/app/user/user-card/user-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../../assets/models/user";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
     selector: 'app-user-card',
@@ -11,12 +12,22 @@ export class UserCardComponent implements OnInit {
     @Input() user?: User;
     @Input() config: UserCardConfig = {} as UserCardConfig
 
-    constructor() {
+    constructor(private translate: TranslateService) {
     }
 
     ngOnInit(): void {
     }
 
+    getTranslatedAccountType() {
+        switch (this.user?.accountType) {
+            case "ADMIN":
+                return this.translate.instant("MAIN_ADMIN");
+            case "MODERATOR":
+                return this.translate.instant("UNIVERSITY_ADMIN");
+            case "USER":
+                return this.translate.instant("USER");
+        }
+    }
 }
 
 export interface UserCardConfig {
